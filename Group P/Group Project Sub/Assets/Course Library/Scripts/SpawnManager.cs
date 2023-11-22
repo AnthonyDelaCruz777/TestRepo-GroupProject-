51ce812a087faae65f3939863fd3e961899f9248
janouskie <b00149565@mytudublin.ie>
2023-11-22 12:54:28 +0000

    alex pls dont touch this
    jannah's 50th reason

---
+++ Group P/Group Project Sub/Assets/Course Library/Scripts/SpawnManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+
+{
+    public GameObject zombiePrefab;
+    private Vector3 spawnPos = new Vector3(-8,0,0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+    public Rigidbody Zrb;
+    Vector3 ImpulseVector = new Vector3(-50.0f,0.0f,0.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnZombie", startDelay, repeatRate);
+        Zrb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnZombie()
+    {
+        Instantiate(zombiePrefab, spawnPos, zombiePrefab.transform.rotation);
+        
+    }
+
+    // void OnCollisionEnter(Collision collide)
+    // {
+    //     if(collide.gameObject.name == "Player")
+    //     {
+    //         //Debug.Log("Player hit by Zombie");
+    //         TakeDamage(2);
+    //         Zrb.AddForce(ImpulseVector,ForceMode.Impulse);
+    //         //rb.velocity = Vector3.zero;
+
+    //     }    
+    // }
+}
