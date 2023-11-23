8a61942db34d6d60af69ba31ebf72027fa937621
AnthonyDelaCruz777 <b00149504@mytudublin.ie>
2023-11-23 16:38:50 +0000

    I am so scared this better not break or we are SO screwed

--- Alex's Work (Test-Alpha)/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Alex's Work (Test-Alpha)/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class SpawnManagerX : MonoBehaviour
 {
@@ -15,7 +19,36 @@ public class SpawnManagerX : MonoBehaviour
     private float startDelay = 1.0f;
     private float spawnInterval = 2.0f;
 
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public GameObject titleScreen;
+    public Button restartButton; 
+
+    public List<GameObject> targetPrefabs;
+
+    // private int score;
+    // private float spawnRate = 1.5f;
+    //public bool isGameActive;
+
+
+
+    /*private float spaceBetweenSquares = 2.5f; 
+    private float minValueX = -3.75f; //  x value of the center of the left-most square
+    private float minValueY = -3.75f; //  y value of the center of the bottom-most square
+    */
+    // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
+
     // Start is called before the first frame update
+
+    /*public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+        isGameActive = true;
+        StartCoroutine(Start());
+
+        titleScreen.SetActive(false);
+    }*/
+
     void Start()
     {
         InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
